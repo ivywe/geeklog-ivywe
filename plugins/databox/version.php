fcdd1c7e6d9d2c0da939e8b2be98426bdd0fd583
Ivy <komma@ivywe.co.jp>
2015-06-19 01:24:33 +0900

    dataBox userBox add math autotag

--- plugins/databox/version.php
+++ plugins/databox/version.php
@@ -1,3 +1,3 @@
 <?php
-$_DATABOX_CONF['version'] = '0.0.0.20150609';
+$_DATABOX_CONF['version'] = '0.0.0.20150618';
 ?>
--- plugins/userbox/version.php
+++ plugins/userbox/version.php
@@ -1,3 +1,3 @@
 <?php
-$_USERBOX_CONF['version'] = '0.0.0.20150609a';
+$_USERBOX_CONF['version'] = '0.0.0.20150618';
 ?>
