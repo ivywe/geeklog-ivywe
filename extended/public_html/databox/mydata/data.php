da46164b1b3fb10e9ec043b1c16bda20d004333c
Ivy <komma@ivywe.co.jp>
2015-12-26 04:04:10 +0900

    dataBox plugin ivywe customise

--- extended/public_html/databox/mydata/data.php
+++ extended/public_html/databox/mydata/data.php
@@ -1434,10 +1434,10 @@ switch ($mode) {
 
 
 }
-$display =COM_startBlock($LANG_DATABOX_ADMIN['piname'],''
-            ,COM_getBlockTemplate('_admin_block', 'header'))
-         .ppNavbarjp($navbarMenu,$LANG_DATABOX_admin_menu[$menuno])
-         .fncMenu($pi_name)
+$display =COM_startBlock( "" ,''
+            , COM_getBlockTemplate('_admin_block', 'header'))
+        // .ppNavbarjp($navbarMenu,$LANG_DATABOX_admin_menu[$menuno])
+        // .fncMenu($pi_name)
          .$display
          .COM_endBlock(COM_getBlockTemplate('_admin_block', 'footer'));
 
