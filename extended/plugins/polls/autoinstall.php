423f8e987cbd75dda8208e1ed3313798e652c471
Ivy <komma@ivywe.co.jp>
2016-02-09 12:16:31 +0900

    extended: Polls plugin: clean

--- extended/plugins/polls/autoinstall.php
+++ extended/plugins/polls/autoinstall.php
@@ -65,7 +65,7 @@ function plugin_autoinstall_polls($pi_name)
         'config.' . $pi_name . '.tab_whatsnew'              => 'Access to configure ' . $pi_name . ' what\'s new block',
         'config.' . $pi_name . '.tab_main'                  => 'Access to configure general ' . $pi_name . ' settings',
         'config.' . $pi_name . '.tab_permissions'           => 'Access to configure ' . $pi_name . ' default permissions',
-        'config.' . $pi_name . '.tab_autotag_permissions'   => 'Access to configure ' . $pi_name . ' autotag usage permissions', 
+        'config.' . $pi_name . '.tab_autotag_permissions'   => 'Access to configure ' . $pi_name . ' autotag usage permissions',
         'config.' . $pi_name . '.tab_poll_block'            => 'Access to configure ' . $pi_name . ' block'
     );
 
@@ -74,8 +74,8 @@ function plugin_autoinstall_polls($pi_name)
         'config.' . $pi_name . '.tab_whatsnew'              => array($pi_admin),
         'config.' . $pi_name . '.tab_main'                  => array($pi_admin),
         'config.' . $pi_name . '.tab_permissions'           => array($pi_admin),
-        'config.' . $pi_name . '.tab_autotag_permissions'   => array($pi_admin), 
-        'config.' . $pi_name . '.tab_poll_block'            => array($pi_admin)        
+        'config.' . $pi_name . '.tab_autotag_permissions'   => array($pi_admin),
+        'config.' . $pi_name . '.tab_poll_block'            => array($pi_admin)
     );
 
     $tables = array(
