69f9f966b80b98d7492c6a1e491f2bfd9985d5cb
Ivy <komma@ivywe.co.jp>
2016-12-20 22:17:01 +0900

    extended: dataBox, userBox plugin update
    Ver 0.0.0.20161219 個別表示 【data】【profile】headercode.thtml
    テーマ変数追加　code,code_withoutlanguage,code_language
    Ver 0.0.0.20161206 サイトマップ XMLSitemap から除外するコード(UserBoxはユーザー名)　対応

--- extended/plugins/databox/version.php
+++ extended/plugins/databox/version.php
@@ -1,3 +1,3 @@
 <?php
-$_DATABOX_CONF['version'] = '0.0.0.20161206' ;
+$_DATABOX_CONF['version'] = '0.0.0.20161219' ;
 ?>
--- extended/plugins/userbox/version.php
+++ extended/plugins/userbox/version.php
@@ -1,3 +1,3 @@
 <?php
-$_USERBOX_CONF['version'] = '0.0.0.20161206' ;
+$_USERBOX_CONF['version'] = '0.0.0.20161219' ;
 ?>
