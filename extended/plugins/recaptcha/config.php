f154cd187085288311fc23fcec7363bd99466b14
Ivy <komma@ivywe.co.jp>
2017-04-30 20:51:48 +0900

    update reCAPTCHA plugin 1.1.5

--- extended/plugins/recaptcha/config.php
+++ extended/plugins/recaptcha/config.php
@@ -37,7 +37,7 @@ if (stripos($_SERVER['PHP_SELF'], basename(__FILE__)) !== false) {
 $_RECAPTCHA_CONF = array();
 
 // Plugin info
-$_RECAPTCHA_CONF['pi_version'] = '1.1.4';					// Plugin Version
+$_RECAPTCHA_CONF['pi_version'] = '1.1.5';					// Plugin Version
 $_RECAPTCHA_CONF['gl_version'] = '1.6.0';					// GL Version plugin for
 $_RECAPTCHA_CONF['pi_url']     = 'https://mystral-kk.net/';	// Plugin Homepage
 $_RECAPTCHA_CONF['GROUPS']     = array(
