da980cfebf7bf482ee925bdfdddfb9b3c4571b8d
Ivy <komma@ivywe.co.jp>
2018-06-27 17:33:22 +0900

    dataBox change 404 error if theme not found

--- extended/plugins/databox/version.php
+++ extended/plugins/databox/version.php
@@ -1,3 +1,3 @@
 <?php
-$_DATABOX_CONF['version'] = '0.0.0.20170410' ;
+$_DATABOX_CONF['version'] = '0.0.0.20180509' ;
 ?>
--- extended/plugins/userbox/version.php
+++ extended/plugins/userbox/version.php
@@ -1,3 +1,3 @@
 <?php
-$_USERBOX_CONF['version'] = '0.0.0.20170410a' ;
+$_USERBOX_CONF['version'] = '0.0.0.20180509' ;
 ?>
