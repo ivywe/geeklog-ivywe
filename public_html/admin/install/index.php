a4314f91ec8f6795424967f2873c10d4885ef529
Ivy <komma@ivywe.co.jp>
2019-04-29 01:03:58 +0900

    install script add function INST_updateSiteUrl  and template form

--- public_html/admin/install/index.php
+++ public_html/admin/install/index.php
@@ -42,3 +42,83 @@ require_once __DIR__ . '/classes/installer.class.php';
 
 $installer = new Installer();
 $installer->run();
+
+/**
+* Fix site_url in content
+*
+* If the site's URL changed due to the migration, this function will replace
+* the old URL with the new one in text content of the given tables.
+*
+* @param    string  $old_url    the site's previous URL
+* @param    string  $new_url    the site's new URL after the migration
+* @param    array   $tablespec  (optional) list of tables to patch
+*
+* The $tablespec is an array of tablename => fieldlist pairs, where the field
+* list contains the text fields to be searched and the table's index field
+* as the first(!) entry.
+*
+* NOTE: This function may be used by plugins during PLG_migrate. Changes should
+*       ensure backward compatibility.
+*
+*/
+function INST_updateSiteUrl($old_url, $new_url, $tablespec = '')
+{
+    global $_TABLES;
+
+    // standard tables to update if no $tablespec given
+    $tables = array(
+        'stories'           => 'sid, introtext, bodytext, related',
+        'storysubmission'   => 'sid, introtext, bodytext',
+        'comments'          => 'cid, comment',
+        'trackback'         => 'cid, excerpt, url',
+        'blocks'            => 'bid, content'
+    );
+
+    if (empty($tablespec) || (! is_array($tablespec))) {
+        $tablespec = $tables;
+    }
+
+    if (empty($old_url) || empty($new_url)) {
+        return;
+    }
+
+    if ($old_url == $new_url) {
+        return;
+    }
+
+    foreach ($tablespec as $table => $fieldlist) {
+        $fields = explode(',', str_replace(' ', '', $fieldlist));
+        $index = array_shift($fields);
+
+        if (empty($_TABLES[$table]) || !DB_checkTableExists($table)) {
+            COM_errorLog("Table $table does not exist - skipping migration");
+            continue;
+        }
+
+        $result = DB_query("SELECT $fieldlist FROM {$_TABLES[$table]}");
+        $numRows = DB_numRows($result);
+        for ($i = 0; $i < $numRows; $i++) {
+            $A = DB_fetchArray($result);
+            $changed = false;
+            foreach ($fields as $field) {
+                $newtxt = str_replace($old_url, $new_url, $A[$field]);
+                if ($newtxt != $A[$field]) {
+                    $A[$field] = $newtxt;
+                    $changed = true;
+                }
+            }
+
+            if ($changed) {
+                $sql = "UPDATE {$_TABLES[$table]} SET ";
+                foreach ($fields as $field) {
+                    $sql .= "$field = '" . DB_escapeString($A[$field]) . "', ";
+                }
+                $sql = substr($sql, 0, -2);
+
+                DB_query($sql . " WHERE $index = '" . DB_escapeString($A[$index]) . "'");
+            }
+        }
+    }
+}
+
+
