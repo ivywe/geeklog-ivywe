619a15be4160ec533c7e13ad2a9e3ee9f662c5e5
ivywe <komma@ivywe.co.jp>
2020-05-31 18:18:18 +0900

    medialist typo

--- extended/system/custom/phpautotags_l.php
+++ extended/system/custom/phpautotags_l.php
@@ -10,6 +10,7 @@
 // | Authors: hiroron         - hiroron AT hiroron DOT com
 // |
 // | Version: 1.0.0 (2017-07-25)
+// | Update: 1.1.0 (2020-05-07) p3引数がプラグイン名なのでp0の全部を加工して返す
 // |
 // +---------------------------------------------------------------------------+
 
@@ -22,10 +23,11 @@ if (strpos(strtolower($_SERVER['PHP_SELF']), 'phpautotags_l.php') !== false) {
 **/
 function phpautotags_l($p1, $p2, $p0, $p3) {
     global $_CONF;
-
     if (empty($p1)) { return ''; }
     if (strcmp($p1, COM_getLanguageId($_CONF['language'])) === 0) {
-        return $p3;
+# 2020/05/07 第4引数のp3にプラグイン名がはいってきているのでp0の全文から加工して返すよう修正
+#        return $p3;
+        return str_replace('[l:'.$p1.']','',str_replace('[/l]','',$p0));
     }
     return '';
 }
